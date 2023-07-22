32fde18b387a4352353a88c13d21bbd68f39c499
Michael Mattingly <mikeamatt1@hotmail.com>
2023-07-22 16:58:59 -0400

    made color and text validation functions

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const convert = require("color-convert");
 
+// This function validates if the logoText input is between 1 and 3 characters
 const logoTextValidation = (input) => {
   if (input.length < 1 || input.length > 3) {
     return "Must be between 1 and 3 characters";
   }
   return true;
+};
+
+// This function validates if the input is a valid color string or hex code
+const colorValidation = (input) => {
+  const colorStrings = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "black",
+    "white",
+  ];
+  if (colorStrings.includes(input.toLowerCase())) {
+    return true;
+  }
+
+  if (/^#[A-Fa-f0-9]{6}|[A-fa-f0-9]{3}$/.test(input)) {
+    return true;
+  }
+
+  return "Must be a valid color string or hex code";
 };
 
 const questions = [
@@ -14,27 +39,29 @@ const questions = [
     name: "logoText",
     message: "What text do you want to use for the logo?",
     validate: logoTextValidation,
-    },
-    {
-        type: "input",
-        name: "logoTextColor",
-        message: "What color do you want the logo text to be?",
-        //TODO: validate color needs to be hex or a color string.
-    },
-    {
-        type: "list",
-        name: "logoShape",
-        message: "What shape do you want the logo to be?",
-        choices: ["circle", "square", "triangle"],
-    },
-    {
-        type: "input",
-        name: "logoBackgroundColor",
-        message: "What color do you want the logo background to be?",
-        //TODO: validate color needs to be hex or a color string.
-    },
+  },
+  {
+    type: "input",
+    name: "logoTextColor",
+    message: "What color do you want the logo text to be?",
+    validate: colorValidation,
+  },
+  {
+    type: "list",
+    name: "logoShape",
+    message: "What shape do you want the logo to be?",
+    choices: ["circle", "square", "triangle"],
+  },
+  {
+    type: "input",
+    name: "logoBackgroundColor",
+    message: "What color do you want the logo background to be?",
+    validate: colorValidation,
+  },
 ];
 
-const init = () => { };
+const init = () => {
+    inquirer.prompt(questions).then(err ? console.log(err) : console.log("Success!"));
+};
 
 init();
