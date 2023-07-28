cd9aa8d7870e07d0a48f722a8abbca7e4df42e5e
Michael Mattingly <mikeamatt1@hotmail.com>
2023-07-27 20:20:36 -0400

    polished code for turnin

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
 const questions = require("./lib/questions");
 const makeLogo = require("./lib/makeLogo.js");
 const writeLogo = require("./lib/writeLogo.js");
@@ -7,7 +6,6 @@ const writeLogo = require("./lib/writeLogo.js");
 const init = async () => {
   const response = await inquirer.prompt(questions);
   const logo = makeLogo(response);
-  console.log(logo);
   writeLogo(logo);
 };
 
